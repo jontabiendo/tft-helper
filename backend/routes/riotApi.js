5ecb016c8aa77c345dfa9c1c0b6b82d2a16eb3e2
JonTabiendo <jontabiendo@gmail.com>
2024-06-03 14:24:53 -0400

    api_key turned to const

--- backend/routes/riotApi.js
+++ backend/routes/riotApi.js
@@ -13,6 +13,8 @@ const axiosNA1 = axios.create({
   header: { "Access-Control-Allow_Origin" : "*"}
 })
 
+const api_key = 'RGAPI-653219d7-1acb-40ef-871a-38cda6fb53a6'
+
 router.get('/:summoner', async function(req, res, next) {
   const start = Date.now();
   console.log("*")
@@ -22,7 +24,7 @@ router.get('/:summoner', async function(req, res, next) {
   console.log("*")
   console.log("*")
   console.log("*")
-  const puuid = await axiosAmericas.get(`/riot/account/v1/accounts/by-riot-id/${req.params.summoner}/NA1?api_key=RGAPI-c6af43e3-1c68-486e-a552-85f7226571a4`)
+  const puuid = await axiosAmericas.get(`/riot/account/v1/accounts/by-riot-id/${req.params.summoner}/NA1?api_key=${api_key}`)
 
   // console.log("*")
   // console.log("*")
@@ -42,7 +44,7 @@ router.get('/:summoner', async function(req, res, next) {
   // console.log("*")
   // console.log("*")
   
-  const summonerInfo = await axiosNA1.get(`/summoner/v1/summoners/by-puuid/${puuidResolved}?api_key=RGAPI-c6af43e3-1c68-486e-a552-85f7226571a4`);
+  const summonerInfo = await axiosNA1.get(`/summoner/v1/summoners/by-puuid/${puuidResolved}?api_key=${api_key}`);
   // console.log("*")
   // console.log("*")
   // console.log("*")
@@ -51,12 +53,12 @@ router.get('/:summoner', async function(req, res, next) {
   // console.log("*")
   // console.log("*")
   const count = 10
-  const matches = await axiosAmericas.get(`/tft/match/v1/matches/by-puuid/${puuidResolved}/ids?count=${count}&api_key=RGAPI-c6af43e3-1c68-486e-a552-85f7226571a4`)
+  const matches = await axiosAmericas.get(`/tft/match/v1/matches/by-puuid/${puuidResolved}/ids?count=${count}&api_key=${api_key}`)
   .then(async e => {
     const fullInfoList = await Promise.all(
       e.data.map(async match => {
         // console.log("match: ", match)
-        const res = await axiosAmericas.get(`/tft/match/v1/matches/${match}?api_key=RGAPI-c6af43e3-1c68-486e-a552-85f7226571a4`)
+        const res = await axiosAmericas.get(`/tft/match/v1/matches/${match}?api_key=${api_key}`)
 
         relevantInfo = normalizeData(res.data.info.participants, puuidResolved)
 
